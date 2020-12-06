ddfbc9396d27b4f0160202f341ca2e14be6d3dbf
RichA <rich_ay@sbcglobal.net>
2020-12-06 08:59:05 -0600

    Updated project files per lesson 13.3.7.

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,68 @@
-const path    = require( "path" );          // we can use Node.js and NPM modules
-const webpack = require( "webpack" );       // needed to use webpack's methods and properties
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const webpack = require("webpack");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+// const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
 
-module.exports = {                          // the main configuration object
-    entry: {
-        app: './assets/js/script.js',         // define the root of the bundle
-        events: "./assets/js/events.js",      // define the entry points for the other modules
-        schedule: "./assets/js/schedule.js",
-        tickets: "./assets/js/tickets.js"
-    },
 
-    output: {                               // specify where the output bundle goes
-        filename: "[name].bundle.js",
-        path: __dirname + "/dist",
-    },
-
-    module: {
-        rules: [
+const config = {
+  entry: {
+    app: './assets/js/script.js',            // define the root of the bundle
+    events: './assets/js/events.js',         // define the entry points for the other modules
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js'
+  },
+  output: {                                  // specify where the output bundle goes
+    filename: '[name].bundle.js',
+    path: __dirname + '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,         // process any "image" files
+        use: [
           {
-            test: /\.jpg$/i,                  // process any 'jpg' file  (file-loader)
-            use: [
-                {
-                  loader: 'file-loader',
-                  options: {
-                    name (file) {
-                      return "[path][name].[ext]"            // return the full pathname of the file
-                    },
-                    publicPath: function(url) {
-                      return url.replace("../", "/assets/")  // replace the ../ from require() with /assets/
-                    }
-                  } 
-                },
-                {
-                    loader: 'image-webpack-loader'
-                }
-              ]
+            loader: 'file-loader',
+            options: {
+              name(file) {
+                return '[path][name].[ext]'; // return the full pathname of the file
+              },
+              publicPath: function(url) {
+                return url.replace('../', '/assets/');  // replace the ../ from require() with /assets/
+              }
+            }
+          },
+          {
+            loader: 'image-webpack-loader'
           }
         ]
-      },
-
-    plugins: [  
-      new webpack.ProvidePlugin({  // define the plugin for JQuery
-        $: "jquery",
-        jQuery: "jquery"
-      }),
-      new BundleAnalyzerPlugin({
-        analyzerMode: "static",    // the report outputs to an HTML file in the dist folder
-      })
-    ],
-    mode: 'development'                      // switch to production when we're finished.
-
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({               // define the plug-in for JQuery
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static'                  // the report outputs to an HTML file in the dist folder
+    })
+    // new WebpackPwaManifest({
+    //   name: "Food Event",
+    //   short_name: "Foodies",
+    //   description: "An app that allows you to view upcoming food events.",
+    //   background_color: "#01579b",
+    //   theme_color: "#ffffff",
+    //   fingerprints: false,
+    //   inject: false,
+    //   icons: [{
+    //     src: path.resolve("assets/img/icons/icon-512x512.png"),
+    //     sizes: [96, 128, 192, 256, 384, 512],
+    //     destination: path.join("assets", "icons")
+    //   }]
+    // })
+  ],
+  mode: 'development'                         // switch to production when we're finished
 };
+
+module.exports = config;
